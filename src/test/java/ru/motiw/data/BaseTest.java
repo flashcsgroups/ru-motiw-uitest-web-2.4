b89e0ac2b337fe2ba86608775870f7f4494b79ee
DogaevProject <boysos@icloud.com>
2018-08-27 21:27:40 +0300

    dev+

--- src/test/java/ru/motiw/data/BaseTest.java
+++ src/test/java/ru/motiw/data/BaseTest.java
@@ -34,7 +34,7 @@ public abstract class BaseTest {
         setDriverByName("chrome");
         /*VideoRecorder.conf()
                 .withVideoFolder("build/reports/video");*/
-        Configuration.baseUrl = http + "motiwtest4.test.lan";
+        Configuration.baseUrl = http + "172.22.244.53";
         Configuration.timeout = 8000;
         TextReport.onSucceededTest = true;
         TextReport.onFailedTest = true;
